6c3e795e82a0fbb269dffc7260b4b8563aefc671
Alepacox <alessandro.pacini@studenti.unicam.it>
2018-07-11 02:23:06 +0200

    Fixed some stuff, plus background script for getting domains saved

---
+++ background.js
@@ -0,0 +1,23 @@
+function loadDomains() {
+    if (localStorage.getItem('token') != null) {
+    var url = new URL('http://localhost:8000/api/domain/getall');
+    url.searchParams.append('token', localStorage.getItem('token'));
+    console.log(url);
+    fetch(url, {
+        method: "GET",
+    }).then(res => res.json()).then(val => {
+        if(val.length >0)
+        localStorage.setItem("domains", val);
+    })
+  }
+}
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.getdomains == "true"){
+            loadDomains();
+        sendResponse({done: "true"});
+        }
+    }
+);
+
+loadDomains();
--- views/popup/popup.js
+++ views/popup/popup.js
@@ -1,20 +1,50 @@
-window.onload = function() {
-    var loginButton= document.getElementById('loginButton');
-    if (localStorage.getItem('token') != null){
-        loginButton.innerHTML= "Logout";
+
+window.onload = function () {
+    var loginButton = document.getElementById('loginButton');
+    if (localStorage.getItem('token') != null) {
+        loginButton.innerHTML = "Logout";
         loginButton.addEventListener('click', logout);
-        document.getElementById('description').innerHTML= "Nessuna password per questo dominio";
+        var description = document.getElementById('description');
+        readDomains(description);
     } else {
         loginButton.addEventListener('click', openLoginPage);
     }
-  }
-
-function openLoginPage(){
-    chrome.tabs.create({url: chrome.extension.getURL("../views/login/login.html")});
 }
 
-function logout(){
-    localStorage.removeItem('token');
-    loginButton.innerHTML= "Login";
-    loginButton.addEventListener('click', openLoginPage);
+
+function readDomains(description) {
+    chrome.tabs.query({ //This method output active URL 
+        "active": true,
+    }, function (tabs) {
+        description.innerHTML = extractHostname(tabs[0].url);
+    });
+}
+
+function extractHostname(url) {
+    var hostname;
+    if (url.indexOf("://") > -1)
+        hostname = url.split('/')[2];
+    else hostname = url.split('/')[0];
+    hostname = hostname.split(':')[0];
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+
+function openLoginPage() {
+    chrome.tabs.create({ url: chrome.extension.getURL("../views/login/login.html") });
+}
+
+function logout() {
+    var url = new URL('http://localhost:8000/api/logout');
+    url.searchParams.append('token', localStorage.getItem('token'));
+    fetch(url, {
+        method: "GET",
+    }).then(res => res.json()).then(val => {
+        localStorage.removeItem('token');
+        loginButton.innerHTML = "Login";
+        loginButton.addEventListener('click', openLoginPage);
+        document.getElementById('description').innerHTML="Effettua il login per iniziare ad usare DominKey";
+        })
+
 }
