46c0ac6dbd3cbac8892557500b112d770543638f
Alepacox <alessandro.pacini@studenti.unicam.it>
2018-07-11 22:37:55 +0200

    Moved most of js on popup for security reason

--- background.js
+++ background.js
@@ -1,22 +1,3 @@
-function loadDomains() {
-    if (localStorage.getItem('token') != null) {
-        var url = new URL('http://localhost:8000/api/domain/getall');
-        url.searchParams.append('token', localStorage.getItem('token'));
-        fetch(url, {
-            method: "GET",
-        }).then(res => res.json()).catch((err) => {
-            console.error("No server connection");
-        }).then(val => {
-            if (!Object.prototype.hasOwnProperty(val, 'authenticated')) {
-                if (val.length > 0)
-                    localStorage.setItem("domains", val);
-            } else {
-                logout(true);
-            }
-        })
-    }
-}
-
 function logout(expired) {
     if (expired == undefined) {
         var url = new URL('http://localhost:8000/api/logout');
@@ -26,21 +7,16 @@ function logout(expired) {
         }).catch((err) => {
             console.log(err);
         });
-        console.log("done");
     }
-    localStorage.removeItem('domains');
-    localStorage.removeItem('token');
+    localStorage.clear();
 }
 
 chrome.runtime.onMessage.addListener(
     function (request) {
-        if (request.getdomains == "true") {
-            loadDomains();
-        }
         if (request.logout == "true") {
-            logout();
+            if (request.expired == "true"){
+                logout(true);
+            } else logout();
         }
     }
-);
-
-loadDomains();
+)
--- views/popup/popup.js
+++ views/popup/popup.js
@@ -4,39 +4,55 @@ window.onload = function () {
     if (localStorage.getItem('token') != null) {
         loginButton.innerHTML = "Logout";
         loginButton.addEventListener('click', sendLogoutMessage);
-        var description = document.getElementById('description');
-        readCurrentDomain(description);
+        document.getElementById('description').innerHTML = "Sto scaricando il caricando il contenuto dalla pagina";
+        checkForSavedDomains();
     } else {
         loginButton.addEventListener('click', openLoginPage);
     }
 }
 
-function sendLogoutMessage(){
-    chrome.runtime.sendMessage({ logout: "true" }, {});
-    loginButton.innerHTML = "Login";
-    loginButton.addEventListener('click', openLoginPage);
-    document.getElementById('description').innerHTML = "Effettua il login per iniziare ad usare DominKey";
-}
-
-
-function readCurrentDomain(description) {
-    chrome.tabs.query({ //This method output active URL 
+function checkForSavedDomains() {
+    chrome.tabs.query({
         "active": true,
     }, function (tabs) {
-        description.innerHTML = extractHostname(tabs[0].url);
+        var domainUrl = tabs[0].url;
+        var url = new URL('http://localhost:8000/api/domain/search');
+        var domain = getCurrentDomain(domainUrl);
+        url.searchParams.append('domain', domain);
+        url.searchParams.append('token', localStorage.getItem('token'));
+        fetch(url, {
+            method: "GET",
+        }).then(res => res.json()).catch((err) => {
+            console.error("No server connection");
+        }).then(val => {
+            if (Object.prototype.hasOwnProperty.call(val, 'authenticated')) {
+                sendLogoutMessage(true);
+            } else if (val.foundDomain == true) {
+                document.getElementById('description').innerHTML = "Hai una password salvata per questo dominio";
+            } else document.getElementById('description').innerHTML = "Nessuna password trovata per questo dominio";
+        });
     });
 }
 
-function extractHostname(url) {
+function getCurrentDomain(tabUrl) {
     var hostname;
-    if (url.indexOf("://") > -1)
-        hostname = url.split('/')[2];
-    else hostname = url.split('/')[0];
+    if (tabUrl.indexOf("://") > -1)
+        hostname = tabUrl.split('/')[2];
+    else hostname = tabUrl.split('/')[0];
     hostname = hostname.split(':')[0];
     hostname = hostname.split('?')[0];
-
     return hostname;
 }
+
+function sendLogoutMessage(expired) {
+    if (expired == true) {
+        chrome.runtime.sendMessage({ logout: "true", expired: "true" }, {});
+    } else chrome.runtime.sendMessage({ logout: "true" }, {});
+    loginButton.innerHTML = "Login";
+    loginButton.addEventListener('click', openLoginPage);
+    document.getElementById('description').innerHTML = "Sessione scaduta, effettua nuovamente il login per utilizzare DominKey";
+}
+
 
 function openLoginPage() {
     chrome.tabs.create({ url: chrome.extension.getURL("../views/login/login.html") });
